681eff0f7b9056f8a8562c8ecdd4c9d46ac63bf0
Unnati Aggarwal <unnati3444@gmail.com>
2026-07-14 19:04:01 +0530

    Fixed Railway port issue

--- server.js
+++ server.js
@@ -59,7 +59,8 @@ ${content}
 
 
 // ✅ Start server
-const PORT = process.env.PORT || 3000
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`)
 })
